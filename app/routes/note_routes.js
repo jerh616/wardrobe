196cf82258ba141b0a4a4f7a0fb295ff8b3e1111
Jerh. O'Connor <jerhoconnor@gmail.com>
2018-10-15 17:39:06 +0100

    CRUD endpoints initial pass complete

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,14 +1,60 @@
 'use strict';
 
-console.log('loaded');
+var ObjectID = require('mongodb').ObjectID;
 
-module.exports = function(app, db) {
+module.exports = function(app, myDB) {
+
     app.post('/notes', (req, res) => {
         // We create a new note here
-        console.log(req.body);
-        res.send('Note create request accepted');
+        const note = { text: req.body.body, title: req.body.title};
+        myDB.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'A DB error has occurred'});
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
     });
+
     app.get('/health', (req, res) => {
         res.send('Johnnie 5 alive!');
+    });
+
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id)};
+        myDB.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'A DB read error has occurred'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id)};
+        myDB.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({'error':'A remove error occurred on DB operation'});
+            } else {
+                res.send('Note ' + id + ' sucessfully deleted');
+            }
+        });
+    });
+
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id' : new ObjectID(id)};
+        const newNote = { text: req.body.body, title: req.body.title};
+
+        myDB.collection('notes').updateOne(details, newNote, (err, item) => {
+            if (err) {
+                res.send({'error':'Error occurred trying to update ' + id});
+            } else {
+                res.send('Note ' + id + ' successfully updated');
+            }
+        });
     });
 };
