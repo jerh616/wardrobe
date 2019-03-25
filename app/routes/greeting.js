1efe15e9ef802961cd95f597f8ab515ab54a8f02
Jerh. O'Connor <16563608+jerh616@users.noreply.github.com>
2019-03-25 09:17:27 +0000

    Remove routes and update .gitignore

--- app/routes/greeting.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    hello: function(req, res) {
-        if (!req.body.name) {
-            res.send('An error occurred: name is a required parameter');
-        }
-    }
-};
--- app/routes/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const noteRoutes = require('./note_routes');
-
-module.exports = function(app, db) {
-    noteRoutes(app, db);
-    // Other route groups could go here in the future
-};
